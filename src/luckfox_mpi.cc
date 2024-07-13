7da2657fdb3da734a2c90fa0f7ade9edbfa06d29
olegovechko <oleg.subzero@gmail.com>
2024-07-13 17:33:13 +0300

    Update luckfox_mpi.cc

--- src/luckfox_mpi.cc
+++ src/luckfox_mpi.cc
@@ -160,23 +160,23 @@ int venc_init(int chnId, int width, int height, RK_CODEC_ID_E enType) {
 
 	// RTSP H265	
 	stAttr.stVencAttr.enType = enType;
-	//stAttr.stVencAttr.enPixelFormat = RK_FMT_YUV420SP;
-	stAttr.stVencAttr.enPixelFormat = RK_FMT_RGB888;
+	stAttr.stVencAttr.enPixelFormat = RK_FMT_YUV420SP;
+	//stAttr.stVencAttr.enPixelFormat = RK_FMT_RGB888;
 	stAttr.stVencAttr.u32Profile = H265E_PROFILE_MAIN;
 	stAttr.stVencAttr.u32PicWidth = width;
 	stAttr.stVencAttr.u32PicHeight = height;
 	stAttr.stVencAttr.u32VirWidth = (width+15)&(~15);
 	stAttr.stVencAttr.u32VirHeight = (height+15)&(~15);
-	stAttr.stVencAttr.u32StreamBufCnt = 2;
-	stAttr.stVencAttr.u32BufSize = width * height * 3 / 2;
+	stAttr.stVencAttr.u32StreamBufCnt = 3;
+	stAttr.stVencAttr.u32BufSize = width * height * 4;
 	stAttr.stVencAttr.enMirror = MIRROR_NONE;
 
 	stAttr.stRcAttr.enRcMode = VENC_RC_MODE_H265CBR;
 	stAttr.stRcAttr.stH265Cbr.u32BitRate = 2 * 1024;
-	stAttr.stRcAttr.stH265Cbr.u32Gop = 1;
+	stAttr.stRcAttr.stH265Cbr.u32Gop = 64;
 
-	stAttr.stGopAttr.enGopMode = VENC_GOPMODE_INIT;
-	stAttr.stGopAttr.s32VirIdrLen = 0;
+	stAttr.stGopAttr.enGopMode = VENC_GOPMODE_SMARTP;
+	stAttr.stGopAttr.s32VirIdrLen = 32;
 	stAttr.stGopAttr.u32MaxLtrCount = 0;
 	stAttr.stGopAttr.u32TsvcPreload = 0;
 	
