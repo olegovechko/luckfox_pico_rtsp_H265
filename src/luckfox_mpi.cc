79be074bfa71710de375f7f781ae8e1f147cb667
olegovechko <oleg.subzero@gmail.com>
2024-07-13 18:29:53 +0300

    Update luckfox_mpi.cc

--- src/luckfox_mpi.cc
+++ src/luckfox_mpi.cc
@@ -172,8 +172,8 @@ int venc_init(int chnId, int width, int height, RK_CODEC_ID_E enType) {
 	stAttr.stVencAttr.enMirror = MIRROR_NONE;
 
 	stAttr.stRcAttr.enRcMode = VENC_RC_MODE_H265CBR;
-	stAttr.stRcAttr.stH265Cbr.u32BitRate = 2 * 1024;
-	stAttr.stRcAttr.stH265Cbr.u32Gop = 16;
+	stAttr.stRcAttr.stH265Cbr.u32BitRate = 3 * 1024;
+	stAttr.stRcAttr.stH265Cbr.u32Gop = 2;
 
 	//stAttr.stGopAttr.enGopMode = VENC_GOPMODE_NORMALP;
 	//stAttr.stGopAttr.s32VirIdrLen = 1;
